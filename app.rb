13f039e91bdbae7963cbd595834079111423f909
Chris Fung <aergonaut@gmail.com>
2013-10-16 12:06:38 -0700

    respond_to formats

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 require 'sinatra/base'
 require 'sinatra/contrib'
+require 'sinatra/reloader'
 
 class App < Sinatra::Base
 
   configure do
+    register Sinatra::Contrib
+
     set :haml, format: :html5
   end
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  respond_to :html, :json
 
   get '/to/you' do
     haml :index, locals: { name: false }
